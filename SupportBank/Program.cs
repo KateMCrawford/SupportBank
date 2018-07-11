534c39fa26ae27ff25078d7f03a3d862d598a985
Katherine Crawford <Katherine.Crawford@softwire.com>
2018-07-11 11:19:01 +0100

    It's not working yet but it's closer

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SupportBank
 {
+    class EmployeeAccount
+    {
+        private string name;
+        private decimal balance;
+
+        public EmployeeAccount(string name)
+        {
+            this.name = name;
+            this.balance = 0;
+        }
+
+        public void ChangeBalance(decimal amount)
+        {
+            this.balance = this.balance + amount;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            using (var reader = new StreamReader(@"C:\Users\KMC\Downloads\SupportBank-master\Transactions2014.csv"))
+            {
+                List<string> fromList = new List<string>();
+                List<string> toList = new List<string>();
+                List<decimal> amountList = new List<decimal>();
+
+                reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    fromList.Add(values[1]);
+                    toList.Add(values[2]);
+                    amountList.Add(Convert.ToDecimal(values[4]));
+                }
+
+                IEnumerable<string> distinctNames = fromList.Concat(toList).ToList().Distinct();
+
+                List<EmployeeAccount> accountList = new List<EmployeeAccount>();
+
+                foreach(string name in distinctNames)
+                {
+                    accountList.Add(new EmployeeAccount(name));
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
