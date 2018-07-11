7447b1c2ceb6bf5b94040fdb85aae4d3338dafed
Katherine Crawford <Katherine.Crawford@softwire.com>
2018-07-11 12:16:48 +0100

    Theoretically working

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -11,16 +11,34 @@ namespace SupportBank
     {
         private string name;
         private decimal balance;
+        private List<string> transactions = new List<string>();
 
         public EmployeeAccount(string name)
         {
             this.name = name;
             this.balance = 0;
         }
-
-        public void ChangeBalance(decimal amount)
+        
+        public void AddTransaction(string date, string nameFrom, string nameTo, string narrative, decimal amount)
         {
-            this.balance = this.balance + amount;
+            transactions.Add("On " + date + ", " + nameFrom + " gave " + nameTo + " £" + Convert.ToString(amount) + " due to " + narrative + ". ");
+            if (nameFrom == this.name)
+                this.balance = this.balance + amount;
+            if (nameTo == this.name)
+                this.balance = this.balance - amount;
+        }
+
+        public void ListTransactions()
+        {
+            for (int i = 0; i < transactions.Count; i++)
+            {
+                Console.WriteLine(transactions[i]);
+            }
+        }
+
+        public void WriteBalance()
+        {
+            Console.WriteLine(this.name + " has balance " + this.balance + ". ");
         }
     }
 
@@ -30,8 +48,10 @@ namespace SupportBank
         {
             using (var reader = new StreamReader(@"C:\Users\KMC\Downloads\SupportBank-master\Transactions2014.csv"))
             {
+                List<string> dateList = new List<string>();
                 List<string> fromList = new List<string>();
                 List<string> toList = new List<string>();
+                List<string> narrativeList = new List<string>();
                 List<decimal> amountList = new List<decimal>();
 
                 reader.ReadLine();
@@ -41,21 +61,53 @@ namespace SupportBank
                     var line = reader.ReadLine();
                     var values = line.Split(',');
 
+                    dateList.Add(values[0]);
                     fromList.Add(values[1]);
                     toList.Add(values[2]);
+                    narrativeList.Add(values[3]);
                     amountList.Add(Convert.ToDecimal(values[4]));
                 }
 
                 IEnumerable<string> distinctNames = fromList.Concat(toList).ToList().Distinct();
 
-                List<EmployeeAccount> accountList = new List<EmployeeAccount>();
+                Dictionary<string, EmployeeAccount> accountList = new Dictionary<string, EmployeeAccount>();
 
-                foreach(string name in distinctNames)
+                foreach (string name in distinctNames)
                 {
-                    accountList.Add(new EmployeeAccount(name));
+                    accountList.Add(name, new EmployeeAccount(name));
                 }
 
-                Console.ReadLine();
+                for (int i = 0; i < dateList.Count; i++)
+                {
+                    accountList[fromList[i]].AddTransaction(dateList[i], fromList[i], toList[i], narrativeList[i], amountList[i]);
+                    accountList[toList[i]].AddTransaction(dateList[i], fromList[i], toList[i], narrativeList[i], amountList[i]);
+                }
+
+                bool foundName = false;
+
+                string input = Console.ReadLine();
+                if (input.ToLower() == "list all")
+                    foreach (string name in distinctNames)
+                    {
+                        accountList[name].WriteBalance();
+                    }
+                else if (input.StartsWith("list "))
+                {
+                    string inputName = input.Substring(5);
+
+                    foreach (string name in distinctNames)
+                    {
+                        if (name.ToLower() == inputName.ToLower())
+                        {
+                            accountList[name].ListTransactions();
+                            foundName = true;
+                        }
+                    }
+                    if (!foundName)
+                        Console.WriteLine("This name is not recognised.");
+                }
+                else
+                    Console.WriteLine("This command was not recognised.");
             }
         }
     }
