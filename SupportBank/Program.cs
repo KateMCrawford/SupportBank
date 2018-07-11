10e8661c7be4987260f51f5f10365939531a5797
Katherine Crawford <Katherine.Crawford@softwire.com>
2018-07-11 13:16:51 +0100

    Working part 1

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 
 namespace SupportBank
 {
+
     class EmployeeAccount
     {
         private string name;
@@ -48,66 +49,69 @@ namespace SupportBank
         {
             using (var reader = new StreamReader(@"C:\Users\KMC\Downloads\SupportBank-master\Transactions2014.csv"))
             {
-                List<string> dateList = new List<string>();
-                List<string> fromList = new List<string>();
-                List<string> toList = new List<string>();
-                List<string> narrativeList = new List<string>();
-                List<decimal> amountList = new List<decimal>();
+                
+                    List<string> dateList = new List<string>();
+                    List<string> fromList = new List<string>();
+                    List<string> toList = new List<string>();
+                    List<string> narrativeList = new List<string>();
+                    List<decimal> amountList = new List<decimal>();
 
-                reader.ReadLine();
+                    reader.ReadLine();
 
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var values = line.Split(',');
-
-                    dateList.Add(values[0]);
-                    fromList.Add(values[1]);
-                    toList.Add(values[2]);
-                    narrativeList.Add(values[3]);
-                    amountList.Add(Convert.ToDecimal(values[4]));
-                }
-
-                IEnumerable<string> distinctNames = fromList.Concat(toList).ToList().Distinct();
-
-                Dictionary<string, EmployeeAccount> accountList = new Dictionary<string, EmployeeAccount>();
-
-                foreach (string name in distinctNames)
-                {
-                    accountList.Add(name, new EmployeeAccount(name));
-                }
-
-                for (int i = 0; i < dateList.Count; i++)
-                {
-                    accountList[fromList[i]].AddTransaction(dateList[i], fromList[i], toList[i], narrativeList[i], amountList[i]);
-                    accountList[toList[i]].AddTransaction(dateList[i], fromList[i], toList[i], narrativeList[i], amountList[i]);
-                }
-
-                bool foundName = false;
-
-                string input = Console.ReadLine();
-                if (input.ToLower() == "list all")
-                    foreach (string name in distinctNames)
+                    while (!reader.EndOfStream)
                     {
-                        accountList[name].WriteBalance();
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        dateList.Add(values[0]);
+                        fromList.Add(values[1]);
+                        toList.Add(values[2]);
+                        narrativeList.Add(values[3]);
+                        amountList.Add(Convert.ToDecimal(values[4]));
                     }
-                else if (input.StartsWith("list "))
-                {
-                    string inputName = input.Substring(5);
+
+                    IEnumerable<string> distinctNames = fromList.Concat(toList).ToList().Distinct();
+
+                    Dictionary<string, EmployeeAccount> accountList = new Dictionary<string, EmployeeAccount>();
 
                     foreach (string name in distinctNames)
                     {
-                        if (name.ToLower() == inputName.ToLower())
+                        accountList.Add(name, new EmployeeAccount(name));
+                    }
+
+                    for (int i = 0; i < dateList.Count; i++)
+                    {
+                        accountList[fromList[i]].AddTransaction(dateList[i], fromList[i], toList[i], narrativeList[i], amountList[i]);
+                        accountList[toList[i]].AddTransaction(dateList[i], fromList[i], toList[i], narrativeList[i], amountList[i]);
+                    }
+
+                    bool foundName = false;
+
+                    string input = Console.ReadLine();
+                    if (input.ToLower() == "list all")
+                        foreach (string name in distinctNames)
                         {
-                            accountList[name].ListTransactions();
-                            foundName = true;
+                            accountList[name].WriteBalance();
                         }
+                    else if (input.StartsWith("list "))
+                    {
+                        string inputName = input.Substring(5);
+                        foreach (string name in distinctNames)
+                        {
+                            if (name.ToLower() == inputName.ToLower())
+                            {
+                                accountList[name].ListTransactions();
+                                foundName = true;
+                            }
+                        }
+                        if (!foundName)
+                            Console.WriteLine("This name is not recognised.");
                     }
-                    if (!foundName)
-                        Console.WriteLine("This name is not recognised.");
-                }
-                else
-                    Console.WriteLine("This command was not recognised.");
+                    else
+                        Console.WriteLine("This command was not recognised.");
+
+                    Console.ReadLine();
+                
             }
         }
     }
